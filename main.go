75e2faf91a08644036a70d588eb535945873b22c
William Kennedy <bill@ardanstudios.com>
2013-08-30 22:27:50 -0400

    Testing Linux

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 /*
 	-- To build this code set this variable and verify the pkg-config tool can find the pkg-config file
-	export PKG_CONFIG_PATH=$HOME/Spaces/PublicPackages/src/github.com/goinggo/keyboard/pkgconfig
+	export PKG_CONFIG_PATH=$GOPATH/src/github.com/goinggo/keyboard/pkgconfig
 	pkg-config --cflags --libs GoingGoKeyboard
 
 	-- To run this code set this variable so the dynamic library can be found at runtime
-	export DYLD_LIBRARY_PATH=$HOME/Spaces/PublicPackages/src/github.com/goinggo/keyboard/DyLib
+	export DYLD_LIBRARY_PATH=$GOPATH/src/github.com/goinggo/keyboard/DyLib
+
+	go get github.com/goinggo/keyboard
 */
 package main
 
