f26b6b10095b84402d56fa93e8e4a7181c4a1503
William Kennedy <bill@ardanstudios.com>
2014-07-16 14:07:49 -0400

    go-link compliant

--- main.go
+++ main.go
@@ -3,27 +3,27 @@
 // license that can be found in the LICENSE file.
 
 /*
-	This program provides sample code that shows you how to bind a dynamic library using CGO.
+This program provides sample code that shows you how to bind a dynamic library using CGO.
 
-	Ardan Studios
-	12973 SW 112 ST, Suite 153
-	Miami, FL 33186
-	bill@ardanstudios.com
+Ardan Studios
+12973 SW 112 ST, Suite 153
+Miami, FL 33186
+bill@ardanstudios.com
 
-	// Set these variable before getting and building the code
-	export GOPATH=$HOME/keyboard
-	export PKG_CONFIG_PATH=$GOPATH/src/github.com/goinggo/keyboard/pkgconfig
-	export DYLD_LIBRARY_PATH=$GOPATH/src/github.com/goinggo/keyboard/DyLib
+// Set these variable before getting and building the code
+export GOPATH=$HOME/keyboard
+export PKG_CONFIG_PATH=$GOPATH/src/github.com/goinggo/keyboard/pkgconfig
+export DYLD_LIBRARY_PATH=$GOPATH/src/github.com/goinggo/keyboard/DyLib
 
-	// Get, build and install the code
-	go get github.com/goinggo/keyboard
+// Get, build and install the code
+go get github.com/goinggo/keyboard
 
-	// Run the code
-	cd $GOPATH/bin
-	./keyboard
+// Run the code
+cd $GOPATH/bin
+./keyboard
 
-	// Show things are set correctly
-	pkg-config --cflags --libs GoingGoKeyboard
+// Show things are set correctly
+pkg-config --cflags --libs GoingGoKeyboard
 */
 package main
 
